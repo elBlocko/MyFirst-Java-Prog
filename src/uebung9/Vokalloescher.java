23049113434d2fae8079bb4f3946757f89ec8471
Marco <marco.braeuer1984@gmail.com>
2021-09-02 11:35:16 +0200

    a22

--- src/uebung9/Vokalloescher.java
+++ src/uebung9/Vokalloescher.java
@@ -1,22 +1,8 @@
 package uebung9;
 
-import javax.swing.JOptionPane;
 
 public class Vokalloescher {
-
-	public static void main(String[] args) {
-	
-	       
-	       
-	       
-        String eingabe = JOptionPane.showInputDialog("Geben Sie einen Satz ein:");      
-        StringBuilder str = new StringBuilder(eingabe);
-           for (int i =0; i< eingabe.length(); i++)   {
-        	   str.append( "a" ).append( "" );           
-        }
-       
-   
-		JOptionPane.showMessageDialog(null, str);
-	}
-
+/*
+	Stringbuilder 
+*/
 }
--- src/uebung9/Zahlensysteme.java
+++ src/uebung9/Zahlensysteme.java
@@ -1,10 +1,17 @@
 package uebung9;
 
+import javax.swing.JOptionPane;
+
 public class Zahlensysteme {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String eingabe = JOptionPane.showInputDialog("Zahl eingeben");	
+		int zahl = Integer.parseInt(eingabe);
+		String bin= Integer.toBinaryString(zahl);
+		String hex= Integer.toHexString(zahl);
 
+
+	
+		JOptionPane.showMessageDialog(null, zahl + "\n"+ bin + "\n"+ hex);
 	}
-
 }
