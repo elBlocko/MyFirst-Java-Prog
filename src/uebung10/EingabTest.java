b30384a0a7f691fc91637682c669a4dd23545901
Marco <marco.braeuer1984@gmail.com>
2021-09-02 13:34:08 +0200

    noma

--- src/uebung10/EingabTest.java
+++ src/uebung10/EingabTest.java
@@ -8,17 +8,21 @@ import javax.swing.JOptionPane;
 public class EingabTest {
 
 	public static void main(String[] args) {
-		Instant startEingabeAufforderung = Instant.now();
-		String eingabe = JOptionPane
-				.showInputDialog("Geben Sie so schnell wie möglich \n 'Fischers Fritz fischt frische Fische' ein");
+
 		String soll = "Fischers Fritz fischt frische Fische";
+		Instant startEingabeAufforderung = Instant.now();
+
+		String eingabe = JOptionPane.showInputDialog("Geben Sie so schnell wie möglich \n" + soll + " ein");
+		Instant endeDerEingabe = Instant.now();
+		long min, sec, mili;
+		Duration dauer = Duration.between(startEingabeAufforderung, endeDerEingabe);
+		min = dauer.toMinutes();
+		sec = dauer.getSeconds() - min * 60;
+		mili = dauer.getNano() / 1000000;
+
 		if (eingabe != null && eingabe.equals(soll)) {
-			System.out.println("FehlerFrei!");
-			Instant endeDerEingabe = Instant.now();
-			Duration dauer = Duration.between(startEingabeAufforderung, endeDerEingabe);
-			System.out.println(
-					"FehlerFrei! Zeit: " + dauer.toMinutes() + " Minuten " + 
-			dauer.toSeconds() + " Sekunden " + dauer.toMillis() + " Milisekunden");
+			System.out.println("FehlerFrei! Zeit: " + min + 
+					" Minuten " + sec + " Sekunden " + mili + " Milisekunden");
 		} else {
 			System.out.println("Eingabe war falsch");
 		}
